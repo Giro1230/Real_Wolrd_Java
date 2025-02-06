7cc3bf351070d262e6e728f9ce9cc8824aa065fa
Giro <giro@nextpay.co.kr>
2025-02-06 19:03:22 +0900

    초기 셋팅

---
+++ src/main/java/com/realworld_java/RealWorldJavaApplication.java
@@ -0,0 +1,13 @@
+package com.realworld_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealWorldJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealWorldJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/realworld_java/RealWorldJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.realworld_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
