176ab9c295e0724dc7a908e449f444531f1945a3
Giro <giro@nextpay.co.kr>
2025-02-08 00:06:37 +0900

    User Controller, Domain TEST 준비

---
+++ src/test/java/com/realworld_java/user/controller/UserControllerTest.java
@@ -0,0 +1,24 @@
+package com.realworld_java.user.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserControllerTest {
+
+    @Test
+    void createUser() {
+    }
+
+    @Test
+    void login() {
+    }
+
+    @Test
+    void getCurrentUser() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+}
---
+++ src/test/java/com/realworld_java/user/domain/UserTest.java
@@ -0,0 +1,16 @@
+package com.realworld_java.user.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    User user;
+
+    public UserTest(User user) {
+        this.user = user;
+    }
+
+    @Test
+    void getId() {}
+}
